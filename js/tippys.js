0030176b5d059ef5b7e17971434eac70369dbe93
Ryan Nealeigh <ryan.nealeigh@liferay.cloud>
2019-10-28 13:12:19 -0700

    commit changes

---
+++ js/tippys.js
@@ -0,0 +1,24 @@
+tippy('#htmlIcon', {
+    followCursor: true,
+    delay: [500, 0],
+    theme: 'material',
+    content: "HTML 5"
+});
+tippy('#cssIcon', {
+    followCursor: true,
+    delay: [500, 0],
+    theme: 'material',
+    content: "CSS 3"
+});
+tippy('#jsIcon', {
+    followCursor: true,
+    delay: [500, 0],
+    theme: 'material',
+    content: "JavaScript"
+});
+tippy('#angularIcon', {
+    followCursor: true,
+    delay: [500, 0],
+    theme: 'material',
+    content: "Angular"
+});
